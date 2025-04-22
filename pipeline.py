6090d1418129508f5be1f4e3588b4286c8a58767
Theoneste NDAYISENGA <ndayisengatheoneste79@gmail.com>
2025-04-23 00:16:37 +0700

    Adding the creditial for Database

--- pipeline.py
+++ pipeline.py
@@ -1,1 +1,15 @@
 import pandas as pd
+import requests
+import io
+import psycopg2
+from requests.auth import HTTPBasicAuth
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+# Kobo credentials connect to the API
+KOBO_USERNAME = os.getenv("KOBO_USERNAME")
+KOBO_USERNAME = os.getenv("KOBO_PASSWORD")
+KOBO_CSV_URL = os.getenv("KOBO_CSV_URL")
